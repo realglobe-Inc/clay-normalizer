9b883c5f9e76e31b9dab0b98eb6d08292ab743c7
Taka Okunishi <okunishinish@gmail.com>
2017-03-14 15:33:11 +0900

    Update travis settings

--- example/example-usage.js
+++ example/example-usage.js
@@ -12,7 +12,7 @@ const { normalize, denormalize } = require('clay-normalizer')
 
   console.log(pointers) // -> [ 'User#1', 'User#2' ]
   console.log(pointed) // -> { User: { '1': { /* ... */ }, '2': { /* ... */ } }, Org: { '2': { /* ... */ } } }
-  console.log(relations) // -> { 'User#1': { org: 'Org#30ac84120ec44c61a1b5f28320910b0b' }, 'User#2': { org: 'Org#1' } }
+  console.log(relations) // -> { 'User#1': { org: 'Org#1' }, 'User#2': { org: 'Org#1' } }
 
   let users = denormalize({ pointers, pointed, relations })
   console.log(users) // -> [ { /* ... */ }, { /* ... */ } ]
